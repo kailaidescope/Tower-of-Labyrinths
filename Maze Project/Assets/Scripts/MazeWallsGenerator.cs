98ce41078783ce73f649622000a02e5204c2526e
m3lodicaman <33330721+m3lodicaman@users.noreply.github.com>
2019-10-16 11:05:14 -0400

    Started maze generation script

--- Maze Project/Assets/Scripts/MazeWallsGenerator.cs
+++ Maze Project/Assets/Scripts/MazeWallsGenerator.cs
@@ -9,6 +9,9 @@ public class MazeWallsGenerator : MonoBehaviour
     [SerializeField] private GameObject emptyPrefab;
 
     private bool[,] walls;
+    private string[] directions;
+    private string[] directions2;
+    private string nextDirection;
     private float xLen;
     private float zLen;
 
@@ -19,15 +22,78 @@ public class MazeWallsGenerator : MonoBehaviour
         zLen = plane.transform.localScale.z * 10f;
 
         walls = new bool[(int)xLen, (int)zLen];
+        directions = new string[]{"down", "right", "down", "right", "both"};
+        directions2 = new string[] { "down", "right", "up", "left", "neither", "neither", "neither" };
 
-        for(int x = 0; x < (int)xLen; x++)
+        walls[0, 0] = true;
+        //walls[x, z] = (Random.value > 0.5f);
+
+        for (int x = 0; x < (int)xLen-1; x++)
+        {
+            for (int z = 0; z < (int)zLen-1; z++)
+            {
+                if (walls[x, z])
+                {
+                    nextDirection = directions[Random.Range(0,directions.Length)];
+                    if(nextDirection == "down")
+                    {
+                        walls[x + 1, z] = true;
+                    }
+                    if (nextDirection == "right")
+                    {
+                        walls[x, z + 1] = true;
+                    }
+                    if (nextDirection == "both")
+                    {
+                        walls[x + 1, z] = true;
+                        walls[x, z + 1] = true;
+                    }
+                }
+            }
+        }
+
+        for (int x = 1; x < (int)xLen - 1; x++)
+        {
+            for (int z = 1; z < (int)zLen - 1; z++)
+            {
+                if (walls[x, z])
+                {
+                    nextDirection = directions[Random.Range(0, directions2.Length)];
+                    if (nextDirection == "down")
+                    {
+                        walls[x + 1, z] = true;
+                    }
+                    if (nextDirection == "right")
+                    {
+                        walls[x, z + 1] = true;
+                    }
+                    if (nextDirection == "up")
+                    {
+                        walls[x - 1, z] = true;
+                    }
+                    if (nextDirection == "right")
+                    {
+                        walls[x, z - 1] = true;
+                    }
+                }
+            }
+        }
+
+        for (int x = 0; x < (int)xLen; x++)
         {
             for(int z = 0; z < (int)zLen; z++)
             {
-                walls[x, z] = (Random.value > 0.5f);
                 if (walls[x, z])
                 {
-                    Instantiate(wallPrefab).transform.position = new Vector3((float)x + 0.5f, 0.5f, (float)z + 0.5f);
+                    GameObject wall = Instantiate(emptyPrefab);
+                    wall.transform.position = new Vector3((float)x + 0.5f, 0.5f, (float)z + 0.5f);
+                    wall.transform.parent = plane.transform;
+                }
+                else
+                {
+                    GameObject wall = Instantiate(wallPrefab);
+                    wall.transform.position = new Vector3((float)x + 0.5f, 0.5f, (float)z + 0.5f);
+                    wall.transform.parent = plane.transform;
                 }
             }
         }
