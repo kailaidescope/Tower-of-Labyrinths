305f86318fee880de5d33c46fc4676507a1b3cac
m3lodicaman <33330721+m3lodicaman@users.noreply.github.com>
2019-10-17 11:00:10 -0400

    Modularized maze gen

--- Maze Project/Assets/Scripts/MazeWallsGenerator.cs
+++ Maze Project/Assets/Scripts/MazeWallsGenerator.cs
@@ -7,6 +7,7 @@ public class MazeWallsGenerator : MonoBehaviour
     [SerializeField] private GameObject plane;
     [SerializeField] private GameObject wallPrefab;
     [SerializeField] private GameObject emptyPrefab;
+    [SerializeField] private int complexity;
 
     private bool[,] walls;
     private string[] directions;
@@ -22,62 +23,21 @@ public class MazeWallsGenerator : MonoBehaviour
         zLen = plane.transform.localScale.z * 10f;
 
         walls = new bool[(int)xLen, (int)zLen];
-        directions = new string[]{"down", "right", "down", "right", "both"};
+        directions = new string[]{"down", "down", "both" , "neither"};
+        
         directions2 = new string[] { "down", "right", "up", "left", "neither", "neither", "neither" };
 
         walls[0, 0] = true;
         //walls[x, z] = (Random.value > 0.5f);
 
-        for (int x = 0; x < (int)xLen-1; x++)
+        for(int i = 0; i < complexity; i++)
         {
-            for (int z = 0; z < (int)zLen-1; z++)
-            {
-                if (walls[x, z])
-                {
-                    nextDirection = directions[Random.Range(0,directions.Length)];
-                    if(nextDirection == "down")
-                    {
-                        walls[x + 1, z] = true;
-                    }
-                    if (nextDirection == "right")
-                    {
-                        walls[x, z + 1] = true;
-                    }
-                    if (nextDirection == "both")
-                    {
-                        walls[x + 1, z] = true;
-                        walls[x, z + 1] = true;
-                    }
-                }
-            }
+            directions = new string[] { "right", "neither" };
+            DrawMaze(0, 0, directions);
+            directions = new string[] { "down", "neither" };
+            DrawMaze(0, 0, directions);
         }
-
-        for (int x = 1; x < (int)xLen - 1; x++)
-        {
-            for (int z = 1; z < (int)zLen - 1; z++)
-            {
-                if (walls[x, z])
-                {
-                    nextDirection = directions[Random.Range(0, directions2.Length)];
-                    if (nextDirection == "down")
-                    {
-                        walls[x + 1, z] = true;
-                    }
-                    if (nextDirection == "right")
-                    {
-                        walls[x, z + 1] = true;
-                    }
-                    if (nextDirection == "up")
-                    {
-                        walls[x - 1, z] = true;
-                    }
-                    if (nextDirection == "right")
-                    {
-                        walls[x, z - 1] = true;
-                    }
-                }
-            }
-        }
+        //DrawMaze(1, 1, directions2);
 
         for (int x = 0; x < (int)xLen; x++)
         {
@@ -103,5 +63,41 @@ public class MazeWallsGenerator : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void DrawMaze(int startX, int startZ, string[] currentDirections)
+    {
+        for (int x = startX; x < (int)xLen - 1; x++)
+        {
+            for (int z = startZ; z < (int)zLen - 1; z++)
+            {
+                if (walls[x, z])
+                {
+                    nextDirection = currentDirections[Random.Range(0, currentDirections.Length)];
+                    Debug.Log(nextDirection);
+                    if (nextDirection == "down")
+                    {
+                        walls[x + 1, z] = true;
+                    }
+                    if (nextDirection == "right")
+                    {
+                        walls[x, z + 1] = true;
+                    }
+                    if (nextDirection == "up" && z > 0)
+                    {
+                        walls[x - 1, z] = true;
+                    }
+                    if (nextDirection == "left" && x > 0)
+                    {
+                        walls[x, z - 1] = true;
+                    }
+                    if (nextDirection == "both")
+                    {
+                        walls[x + 1, z] = true;
+                        walls[x, z + 1] = true;
+                    }
+                }
+            }
+        }
     }
 }
