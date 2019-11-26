75068acd1233c05c39932ecff0d8de58a8b4179b
BlueTitanium <miaht1@bxscience.edu>
2019-11-26 11:07:09 -0500

    Added some stuff in the battle scene to test

---
+++ Maze Project/Assets/Scripts/AIMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour {       
+    public Transform goal;
+    NavMeshAgent agent;
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Update() {
+        agent.destination = goal.position;     
+    }
+}
---
+++ Maze Project/Assets/Scripts/PlayerTestScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestScript : MonoBehaviour
+{
+    Rigidbody rb3d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb3d = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Enemy"){
+            Debug.Log("Enter Battle");
+        }
+    }
+}
