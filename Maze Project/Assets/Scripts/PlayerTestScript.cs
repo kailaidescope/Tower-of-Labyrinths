e8b94922ff9750396d8ecbb8b569d95562fcb2bc
BlueTitanium <miaht1@bxscience.edu>
2019-12-09 11:03:46 -0500

    Implemented Switching from Move to Battle

--- Maze Project/Assets/Scripts/PlayerTestScript.cs
+++ Maze Project/Assets/Scripts/PlayerTestScript.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class PlayerTestScript : MonoBehaviour
 {
     Rigidbody rb3d;
+    public Camera MoveCam, BattleCam;
     // Start is called before the first frame update
     void Start()
     {
         rb3d = GetComponent<Rigidbody>();
+        BattleCam.enabled = false; 
+        MoveCam.enabled = true;
     }
 
     // Update is called once per frame
@@ -19,6 +22,15 @@ public class PlayerTestScript : MonoBehaviour
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.tag == "Enemy"){
             Debug.Log("Enter Battle");
+            SwitchCameras();
+            Destroy(other.gameObject.GetComponent<Rigidbody>());
+            other.gameObject.GetComponent<NavMeshAgent>().enabled = false;
+            other.gameObject.GetComponent<AIMovement>().enabled = false;
+            other.gameObject.transform.position = new Vector3(389.67f, 132.6f, -189.16f);
         }
+    }
+    public void SwitchCameras(){
+        BattleCam.enabled = !BattleCam.enabled;
+        MoveCam.enabled = !MoveCam.enabled;
     }
 }
