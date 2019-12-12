7f369a7006c8fd7524a9e5821e2880a40b1d13f6
KAI TURANSKI <TURANSKIK>
2019-12-12 11:15:56 -0500

    ajshflen

---
+++ Maze Project/Assets/Scripts/ExitBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitBehaviour : MonoBehaviour
+{
+    //1245526462146146136136661664661636616366163616515615115146123562344
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
