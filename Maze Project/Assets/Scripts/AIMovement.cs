ee013172a3ca65be9c55b57aa942fffd9b7dc042
m3lodicaman <33330721+m3lodicaman@users.noreply.github.com>
2020-01-07 11:08:57 -0500

    fjeincfe

--- Maze Project/Assets/Scripts/AIMovement.cs
+++ Maze Project/Assets/Scripts/AIMovement.cs
@@ -3,15 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AIMovement : MonoBehaviour {       
+
+public class AIMovement : MonoBehaviour {    
+   
     public Transform goal;
     NavMeshAgent agent;
     public float[] enemyAttackID; 
     public float enemyID = 000f;
-    void Start () {
+    [SerializeField] private float detectRadius;
+    [SerializeField] private float followRadius;
+
+    private bool isFollowing;
+
+    private void Awake()
+    {
+        goal = GameObject.Find("Player").transform;
+    }
+
+    void Start()
+    {
         agent = GetComponent<NavMeshAgent>();
     }
-    void Update() {
-        agent.destination = goal.position;     
+
+
+    void FixedUpdate() {
+
+        if (isFollowing)
+        {
+            if(Vector3.Distance(transform.position, goal.position) > followRadius)
+            {
+                isFollowing = false;
+            }
+        }
+        else
+        {
+            Physics.Raycast(new Ray(transform.position, goal.position), 30);
+        }
+
+        if (isFollowing)
+        {
+            if(agent.isStopped)
+            {
+                agent.isStopped = false; 
+            }
+            agent.destination = goal.position;
+        }
+        else
+        {
+            if (!agent.isStopped)
+            {
+                agent.isStopped = true;
+            }
+        }
     }
 }
